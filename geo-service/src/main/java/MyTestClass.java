89e1e6e4f302691e13f10db2267aa817d54439fb
Angelina <angelina.rubantsova@gmail.com>
2024-04-20 13:46:06 +0200

    Update project

--- geo-service/src/main/java/MyTestClass.java
+++ geo-service/src/main/java/MyTestClass.java
@@ -1,11 +1,12 @@
-package ru.netology.sender;
 
-import org.mockito.Mockito;
 import org.testng.annotations.Test;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
+import ru.netology.geo.GeoServiceImpl;
 import ru.netology.i18n.LocalizationService;
+import ru.netology.i18n.LocalizationServiceImpl;
+import ru.netology.sender.MessageSenderImpl;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -15,8 +16,21 @@ import static org.testng.Assert.assertNotNull;
 import java.util.HashMap;
 import java.util.Map;
 
+public class MyTestClass {
 
-public class MessageSenderImplTest {
+    @Test
+    public void testByIp_RussianIp() {
+        GeoServiceImpl geoService = new GeoServiceImpl();
+        Location location = geoService.byIp("172.1.1.1");
+        assertEquals(Country.RUSSIA, location.getCountry());
+    }
+
+    @Test
+    public void testLocale_RussianCountry() {
+        LocalizationServiceImpl localizationService = new LocalizationServiceImpl();
+        String localizedText = localizationService.locale(Country.RUSSIA);
+        assertEquals("Добро пожаловать", localizedText);
+    }
 
     @Test
     public void testSendRussianMessage() {
